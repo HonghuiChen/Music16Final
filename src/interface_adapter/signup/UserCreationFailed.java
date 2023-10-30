065866f4ed938e313c6e0f691374340776527522
Nguyen Binh Son <sontheteacher0605@gmail.com>
2023-10-30 10:45:45 -0400

    deleted src.main.java

--- src/interface_adapter/signup/UserCreationFailed.java
+++
@@ -1,7 +0,0 @@
-package main.java.interface_adapter.signup;
-
-public class UserCreationFailed extends RuntimeException {
-    public UserCreationFailed(String error) {
-        super(error);
-    }
-}
