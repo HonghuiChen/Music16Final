76998fcf5f7ff8f7e9048686a6ad79ac5d12db4b
HonghuiChen <willhh.chen@mail.utoronto.ca>
2023-11-11 20:31:05 -0500

    Create SearchArtistInterator class. (not done)

--- src/use_case/SearchArtist/SearchArtistInputBoundary.java
+++ src/use_case/SearchArtist/SearchArtistInputBoundary.java
@@ -1,5 +1,7 @@
 package use_case.SearchArtist;
 
+import java.io.IOException;
+
 public interface SearchArtistInputBoundary {
-    void searchArtist(SearchArtistInputData searchArtistInputData);
+    void searchArtist(SearchArtistInputData searchArtistInputData) throws IOException;
 }
