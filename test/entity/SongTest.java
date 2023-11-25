a9ddbca3a783a29b99b31cf44bdaf6d9b600ac22
Nguyen Binh Son <sontheteacher0605@gmail.com>
2023-11-24 19:21:35 -0500

    added test case for entity

--- test/entity/SongTest.java
+++ test/entity/SongTest.java
@@ -1,4 +1,39 @@
+package entity;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class SongTest {
-  
+    @Test
+    void testConstructorAndGetters() {
+        Song song = new Song("Shape of You", "Ed Sheeran", "Divide", "some_url");
+        assertEquals("Shape of You", song.getName());
+        assertEquals("Ed Sheeran", song.getArtist());
+        assertEquals("Divide", song.getAlbum());
+        assertEquals("some_url", song.getUrl());
+    }
+
+    @Test
+    void testAddComments() {
+        Song song = new Song("Yesterday", "The Beatles", "Help!", "another_url");
+        song.addComments("Great song!");
+        song.addComments("My favorite Beatles track.");
+        assertTrue(song.getComments().contains("Great song!"));
+        assertTrue(song.getComments().contains("My favorite Beatles track."));
+    }
+
+    @Test
+    void testNullValuesInConstructor() {
+        assertDoesNotThrow(() -> new Song(null, null, null, null));
+    }
+
+    @Test
+    void testAddNullComment() {
+        Song song = new Song("Lose Yourself", "Eminem", "8 Mile", "url_here");
+        assertDoesNotThrow(() -> song.addComments(null));
+        // Further assertions can be added based on how null comments should be handled
+    }
 }
--- test/entity/UserTest.java
+++ test/entity/UserTest.java
@@ -1,4 +1,40 @@
+package entity;
+
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalDateTime;
+
 import static org.junit.jupiter.api.Assertions.*;
+
 class UserTest {
-  
+
+    @Test
+    void testUserInitialization() {
+        LocalDateTime creationTime = LocalDateTime.now();
+        User user = new User("testUser", "password123", creationTime);
+
+        assertNotNull(user, "User object should not be null");
+        assertEquals("testUser", user.getUsername(), "Username should match the one provided");
+        assertEquals("password123", user.getPassword(), "Password should match the one provided");
+        assertEquals(creationTime, user.getCreationTime(), "Creation time should match the one provided");
+    }
+
+    @Test
+    void testGetUsername() {
+        User user = new User("username", "password", LocalDateTime.now());
+        assertEquals("username", user.getUsername(), "getUsername should return the correct username");
+    }
+
+    @Test
+    void testGetPassword() {
+        User user = new User("username", "password", LocalDateTime.now());
+        assertEquals("password", user.getPassword(), "getPassword should return the correct password");
+    }
+
+    @Test
+    void testGetCreationTime() {
+        LocalDateTime now = LocalDateTime.now();
+        User user = new User("username", "password", now);
+        assertEquals(now, user.getCreationTime(), "getCreationTime should return the correct creation time");
+    }
 }
