7a7f0b61bdd044194eb33b634d870d0cae74ec29
nessagbrla <vanessagabriela.kwok@mail.utoronto.ca>
2023-12-02 17:46:44 -0500

    corrected the ListArtistsOutputDataTest

--- test/use_case/LikeArtists/LikeArtistsOutputDataTest.java
+++ test/use_case/LikeArtists/LikeArtistsOutputDataTest.java
@@ -1,41 +1,14 @@
 package use_case.LikeArtists;
 
-import entity.User;
-
-import java.io.FileNotFoundException;
-import java.time.LocalDateTime;
-import java.time.Month;
-import java.util.ArrayList;
-
-import static org.mockito.ArgumentMatchers.any;
-import static org.mockito.ArgumentMatchers.anyString;
-import static org.mockito.Mockito.*;
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.Test;
 
 public class LikeArtistsOutputDataTest {
-
-    void successTest() throws FileNotFoundException {
-        User userMock = mock(User.class);
-        LikeArtistsDataAccessInterface MockData = mock(LikeArtistsDataAccessInterface.class);
-        LikeArtistsOutputBoundary MockOutput = mock(LikeArtistsOutputBoundary.class);
-
-        LikeArtistsInteractor interactor = new LikeArtistsInteractor(MockData, MockOutput);
-
-        when(MockData.readCurrUser("currentUser.txt")).thenReturn("testUser");
-        LocalDateTime mockDateTime = LocalDateTime.of(2015, Month.JULY, 29, 19, 30, 40);
-        when(MockData.get("testUser")).thenReturn(new User("testUser", "1234", mockDateTime));
-        when(MockData.existsByArtists("testuser", "Ed Sheeran")).thenReturn(false);
-        ArrayList<String> artists = new ArrayList<>();
-        artists.add("Ed Sheeran");
-        when(userMock.getFavoriteArtist()).thenReturn(artists);
-
-        LikeArtistsInputData input = new LikeArtistsInputData("Ed Sheeran");
-
-        interactor.like(input);
-        interactor.unlike(input);
-
-        verify(MockOutput).prepareLikeSuccessView(any(LikeArtistsOutputData.class));
-        verify(MockOutput).prepareUnlikeSuccessView(any(LikeArtistsOutputData.class));
-        verify(MockOutput, times(0)).prepareFailView(anyString());
-
+    LikeArtistsOutputData output = new LikeArtistsOutputData("NIKI", false);
+    @Test
+    void getArtist(){
+        assertEquals("NIKI", output.getArtists());
     }
 }
+
+
