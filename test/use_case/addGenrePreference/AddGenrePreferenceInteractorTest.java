392b4be96e087b218dcc9e3fe61473bf8c982ff2
nauraizzahtaufiq <naura.taufiq@mail.utoronto.ca>
2023-12-02 18:33:05 -0500

    corrected AddGenrePreferenceInteractorTest

--- test/use_case/addGenrePreference/AddGenrePreferenceInteractorTest.java
+++ test/use_case/addGenrePreference/AddGenrePreferenceInteractorTest.java
@@ -1,10 +1,16 @@
 package use_case.addGenrePreference;
 
+import app.api.Token;
 import entity.User;
+import interface_adapter.GenrePreference.GenrePresenter;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.Mock;
 import use_case.addGenrePreference.*;
+import use_case.login.LoginOutputBoundary;
 
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.time.LocalDateTime;
 import java.time.Month;
 import java.util.ArrayList;
@@ -13,17 +19,19 @@ import static org.mockito.Mockito.*;
 
 public class AddGenrePreferenceInteractorTest {
 
+    @BeforeEach
+    void setUp() {
+        Token.main(new String[]{""});
+    }
     @Test
     void successTest() throws FileNotFoundException {
         User userMock = mock(User.class);
         addGenrePreferenceDataAccessInterface dataAccessMock = mock(addGenrePreferenceDataAccessInterface.class);
         addGenrePreferenceOutputBoundary outputBoundaryMock = mock(addGenrePreferenceOutputBoundary.class);
 
-        // Create the interactor
         addGenrePreferenceInteractor interactor =
                 new addGenrePreferenceInteractor(dataAccessMock, outputBoundaryMock);
 
-        // Mock data and behavior
         when(dataAccessMock.readCurrUser("currentUser.txt")).thenReturn("testUser");
         LocalDateTime mockDateTime = LocalDateTime.of(2015, Month.JULY, 29, 19, 30, 40);
         when(dataAccessMock.get("testUser")).thenReturn(new User("testUser", "1234", mockDateTime));
@@ -33,13 +41,10 @@ public class AddGenrePreferenceInteractorTest {
         genres.add("rock");
         when(userMock.getGenrePreference()).thenReturn(genres);
 
-        // Create input data
         addGenrePreferenceInputData inputData = new addGenrePreferenceInputData("pop");
 
-        // Execute the method
         interactor.execute(inputData);
 
-        // Verify interactions and assertions
         verify(outputBoundaryMock).prepareAddSuccessView(any(addGenrePreferenceOutputData.class));
         verify(outputBoundaryMock, times(0)).prepareAddFailView(anyString());
         verify(outputBoundaryMock, times(0)).switchView();
@@ -65,17 +70,59 @@ public class AddGenrePreferenceInteractorTest {
         genres.add("rock");
         when(userMock.getGenrePreference()).thenReturn(genres);
 
-        // Create input data with a genre that is already added
         addGenrePreferenceInputData inputData = new addGenrePreferenceInputData("pop");
 
-        // Execute the method
         interactor.execute(inputData);
 
-        // Verify interactions and assertions
         verify(outputBoundaryMock).prepareAddFailView("Genre already inputted.");
         verify(outputBoundaryMock, times(0)).prepareAddSuccessView(any(addGenrePreferenceOutputData.class));
         verify(outputBoundaryMock, times(0)).switchView();
     }
 
+    @Test
+    void invalidGenreTest() throws FileNotFoundException {
+        addGenrePreferenceDataAccessInterface dataAccessMock = mock(addGenrePreferenceDataAccessInterface.class);
+        addGenrePreferenceOutputBoundary outputBoundaryMock = mock(addGenrePreferenceOutputBoundary.class);
+
+        addGenrePreferenceInteractor interactor =
+                new addGenrePreferenceInteractor(dataAccessMock, outputBoundaryMock);
+
+        when(dataAccessMock.readCurrUser("currentUser.txt")).thenReturn("testUser");
+        LocalDateTime mockDateTime = LocalDateTime.of(2015, Month.JULY, 29, 19, 30, 40);
+        when(dataAccessMock.get("testUser")).thenReturn(new User("testUser", "1234", mockDateTime));
+
+        addGenrePreferenceInputData inputData = new addGenrePreferenceInputData("aeiou");
+
+        interactor.execute(inputData);
+
+        verify(outputBoundaryMock).prepareAddFailView("This is not a valid genre.");
+        verify(outputBoundaryMock, times(0)).prepareAddSuccessView(any(addGenrePreferenceOutputData.class));
+        verify(outputBoundaryMock, times(0)).switchView();
+    }
+
+    @Test
+    void testSwitchView() {
+        addGenrePreferenceDataAccessInterface dataAccessMock = mock(addGenrePreferenceDataAccessInterface.class);
+        addGenrePreferenceOutputBoundary outputBoundaryMock = mock(addGenrePreferenceOutputBoundary.class);
+        GenrePresenter genrePresenterMock = mock(GenrePresenter.class);
+
+        addGenrePreferenceInteractor interactor =
+                new addGenrePreferenceInteractor(dataAccessMock, outputBoundaryMock);
+
+        interactor.switchView();
+        verify(outputBoundaryMock).switchView();
+    }
+
+    @Test
+    void testCancel() {
+        addGenrePreferenceDataAccessInterface dataAccessMock = mock(addGenrePreferenceDataAccessInterface.class);
+        addGenrePreferenceOutputBoundary outputBoundaryMock = mock(addGenrePreferenceOutputBoundary.class);
+
+        addGenrePreferenceInteractor interactor =
+                new addGenrePreferenceInteractor(dataAccessMock, outputBoundaryMock);
+
+        interactor.cancel();
+        verify(outputBoundaryMock).cancel();
+    }
     }
 
