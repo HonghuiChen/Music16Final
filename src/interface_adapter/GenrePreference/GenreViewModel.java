410f41ad1f0aefb0508b0e2a03cff0e3f3782ac0
nauraizzahtaufiq <naura.taufiq@mail.utoronto.ca>
2023-12-01 21:23:23 -0500

    modified small detail in GenreViewModel

--- src/interface_adapter/GenrePreference/GenreViewModel.java
+++ src/interface_adapter/GenrePreference/GenreViewModel.java
@@ -6,7 +6,6 @@ import java.beans.PropertyChangeListener;
 import java.beans.PropertyChangeSupport;
 
 public class GenreViewModel extends ViewModel {
-    public final String TITLE_LABEL = "Genre Preference View";
 
     public static final String ADD_BUTTON_LABEL = "Add";
     public static final String DELETE_BUTTON_LABEL = "Delete";
